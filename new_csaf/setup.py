a94f0943541a21a270b753577979989b98e84497
Joe Eappen <jeappen@purdue.edu>
2021-12-06 17:24:19 -0500

    update setup

--- new_csaf/setup.py
+++ new_csaf/setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='new_csaf',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='gward@python.net',
-      url='https://www.python.org/sigs/distutils-sig/',
+      version='1.1',
+      description='New CSAF no ZMQ with hack for cloudpickle, ray',
+      author='GaloisInc',
+      author_email='',
+      url='https://github.com/yokian/csaf',
       packages=['csaf','csaf.core','csaf.test', 'f16lib','f16lib.models','f16lib.models.helpers','f16lib.models.trained_models'],
      )
